f6ac45575ade221689f181b56e112c181234621f
Navdeep Natt <nattnavdeep41@gmail.com>
2025-04-22 09:36:41 -0500

    Testing basic Postgres stuff, I'm going to start working on Backend

--- Backend/Postgres_DB_conn.py
+++ Backend/Postgres_DB_conn.py
@@ -1,15 +1,9 @@
 import psycopg2 as ps
 
-def create_conn_cursor() ->  ps.cursor:
-    """
-    Creates a connection to the PostgreSQL DB and returns a cursor
-
-    return: pyscopg2.cursor 
-    """
-    # We're too cool to use try-catch 😎
+def create_conn_cursor():
     with open("../resources/PostgreSQL_Login.txt", 'r') as f:
-        dbname: str = f.readline()
-        user: str = f.readline()
+        dbname = f.readline().strip()
+        user = f.readline().strip()
     conn = ps.connect(f"dbname={dbname} user={user}")
-    return conn.cursor() 
-        
+    return conn, conn.cursor()
+
---
+++ Backend/db_setup.py
@@ -0,0 +1,5 @@
+"""
+Sets up all of the tables on Postgres; that is, if none of them are there.
+It will create all of the tables that DON'T exist; making sure not to overwrite
+what's already there. 
+"""
